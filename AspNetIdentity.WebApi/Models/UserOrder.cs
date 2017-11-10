84c72d88fff4b6b224929c6818264f0277a2e515
Krzysztof Koziarski <krzyhook@gmail.com>
2017-11-10 11:15:31 +0100

    Models created - XUser and UserOrders

---
+++ AspNetIdentity.WebApi/Models/UserOrder.cs
@@ -0,0 +1,16 @@
+﻿namespace AspNetIdentity.WebApi.Models
+{
+    using System;
+
+    public class UserOrder
+    {
+        public long Id { get; set; }
+
+        public string OrderName { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public long UserId { get; set; }
+        public virtual XUser User { get; set; }
+    }
+}
---
+++ AspNetIdentity.WebApi/Models/XUser.cs
@@ -0,0 +1,20 @@
+﻿namespace AspNetIdentity.WebApi.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class XUser
+    {
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public virtual ICollection<UserOrder> UserOrders { get; set; }
+    }
+}
